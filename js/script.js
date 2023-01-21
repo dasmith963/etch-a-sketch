eab529b61af6ef56ba8540c75aed9105b50fed37
Devon <Devon.smith017@gmail.com>
2023-01-20 21:46:58 -0600

    Add pseudocode for planning.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Create a 16x16 grid of square divs.
+
+// Set up a hover effect so that the grid divs change color when the mouse passes over them
+  // default color = black
+  // reset() = white
+  // color-picker: color value 
+  // random RGB value
+  // add 10% of black(or white) to color so that only after 10 passes is the square completely black (or white).
+  
+
+// Add a function to change number of squares per side for the new grid. 
+  //  The existing grid should be removed and a new grid should be generated in the same total space as before 
